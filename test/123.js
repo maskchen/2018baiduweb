82ad06e49d0bbc3f1146bc07dcfb04ba3899d904
maskchen <38685541+maskchen@users.noreply.github.com>
2018-07-22 15:12:08 +0800

    test

--- test/123.js
+++ test/123.js
@@ -4,12 +4,16 @@ var sjk, biao;
 x.onupgradeneeded = function (ev) {
     sjk = ev.target.result;
     biao = sjk.createObjectStore('person', {keyPath: 'pin'}); 
-    var t = sjk.transaction(['person'], 'readwrite').objectStore('person');
-    t.add('wuyi', 33);
-    t.add('re', 55);
+    
     console.log(sjk);
     console.log(biao);
-    
+}
+
+x.onsuccess = function () {
+    var t = x.result.transaction(['person'], 'readwrite').objectStore('person');
+    t.add('wuyi', 33);
+    t.add('re', 55);
+    console.log(t);
 }
 
 
